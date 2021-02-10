7a8779718d070845249e5427d090a458bc3ff863
Thor Weinreich <tweinreich@users.noreply.github.com>
2021-02-10 23:52:01 +0100

    Update main.py
    - added separated export buttons for CSV and HTML
    - added messages to tell where export has been saved to
    - added study progress indicator
    - refactored deletion of sections to use groups

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import mechanicalsoup
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -7,7 +8,7 @@ from dearpygui.core import *
 from dearpygui.simple import *
 
 base_url = 'https://virtuohm.ohmportal.de'
-url = base_url + '/pls/portal/'
+portal_url = base_url + '/pls/portal/'
 links = {}
 semester_grades = pd.DataFrame()
 all_grades = pd.DataFrame()
@@ -54,7 +55,55 @@ def prettify_html(filename):
     head.insert(1, css)
     html = soup.find('html')
     html.insert(1, head)
-    with open(Path("./" + filename), 'w') as f: f.write(soup.prettify(formatter='html'))
+    with open(Path("./" + filename), 'w') as f:
+        f.write(soup.prettify(formatter='html'))
+
+
+def calculate_current_credits(dataframe):
+    without_total = dataframe.iloc[0:len(dataframe) - 1]
+    return without_total['ECTS'].sum()
+
+
+def delete_elements(elements_to_delete):
+    """Check for several widgets presence before deleting them if they are shown"""
+    for name in elements_to_delete:
+        shown = is_item_shown(name)
+        if shown:
+            delete_item(name)
+
+
+def file_has_content(path):
+    return Path(path).stat().st_size > 0
+
+
+def get_semester_grades(url):
+    content = browser.follow_link(link=url)
+    table = content.soup.find('table')
+    dataframe = table_to_dataframe(table)
+    dataframe['Note'] = interpret_current_grades(dataframe)
+    dataframe = dataframe.loc[:, ~dataframe.columns.str.contains('^Unnamed')]
+
+    global semester_grades
+    semester_grades = dataframe
+
+    delete_elements(['all-grades'])
+
+    with group('semester', parent='Main Window'):
+        add_dummy(parent='semester', height=10)
+        add_table("Table##semester",
+                  ["Module", "Type", "Grade"],
+                  parent="semester",
+                  show=True)
+        set_table_data("Table##semester", dataframe_to_gui_table(dataframe))
+        add_button('Export Semester Grades to CSV',
+                   callback=file_export_csv,
+                   parent="semester",
+                   tip='Saves your grades to CSV')
+        add_same_line(parent="semester")
+        add_button('Export Semester Grades to HTML',
+                   callback=file_export_html,
+                   parent="semester",
+                   tip='Saves your grades to HTML')
 
 
 def get_all_grades(url):
@@ -67,12 +116,48 @@ def get_all_grades(url):
     global all_grades
     all_grades = dataframe
 
-    add_table("Table##all", ["", "PNr", "Exam", "Stat", "Grade", "ECTS", "SWS", "Note", "Recognition", "Semester"],
-              parent="Main Window", height=600)
-    set_table_data("Table##all", dataframe_to_gui_table(dataframe))
-    add_button('Export All Grades to Files',
-               callback=file_export,
-               parent="Main Window", tip='Saves your grades to CSV and HTML')
+    credits_total = calculate_current_credits(dataframe)
+    finished_percentage = math.floor(credits_total / (210.0 / 100))
+    finished_percentage_float = math.floor((finished_percentage / 100.0) * 10) / 10
+
+    delete_elements(['semester'])
+
+    with group('all-grades', parent="Main Window"):
+        add_dummy(parent="all-grades", height=10)
+        add_table("Table##all",
+                  ["PNr", "Exam", "Stat", "Grade", "ECTS", "SWS", "Note", "Recognition", "Semester"],
+                  parent="all-grades",
+                  height=400,
+                  show=True)
+        add_button('Export All Grades to CSV',
+                   callback=file_export_csv,
+                   parent="all-grades",
+                   tip='Saves your grades to CSV')
+        add_same_line(parent="all-grades")
+        add_button('Export All Grades to HTML',
+                   callback=file_export_html,
+                   parent="all-grades",
+                   tip='Saves your grades to HTML')
+        add_dummy(parent="all-grades", height=10)
+        add_text("Credits: " + str(credits_total),
+                 tip='Calculated total including current achievements',
+                 parent="all-grades")
+        add_dummy(parent="all-grades", height=10)
+        add_text('Study Progress', parent='all-grades')
+        add_same_line(parent="all-grades")
+        add_progress_bar("study-progress",
+                         overlay=str(finished_percentage) + '%',
+                         parent="all-grades")
+        set_value("study-progress", finished_percentage_float)
+        set_table_data("Table##all", dataframe_to_gui_table(dataframe))
+
+
+def close_export_finished_window(sender, data):
+    delete_elements(['File Saved'])
+
+
+def close_export_failed_window(sender, data):
+    delete_elements(['Error saving file!'])
 
 
 def dataframe_to_gui_table(dataframe):
@@ -89,11 +174,43 @@ def dataframe_to_gui_table(dataframe):
     return tabledata
 
 
+def export_finished(path):
+    absolute_path = Path(path).resolve()
+    with window('File Saved', autosize=True, no_resize=True):
+        add_text('Your export has been saved to: ' + str(absolute_path))
+        add_button('Ok', callback=close_export_finished_window)
+    set_render_callback(position_export_window)
+
+
+def export_error():
+    with window('Error saving file!', autosize=True, no_resize=True):
+        add_text('There was an error saving your file.')
+        add_button('Ok', callback=close_export_failed_window)
+
+
 def export_dataframe_to_files(dataframe, filename):
     """Writes a dataframe to CSV and prettified HTML"""
+    export_dataframe_to_csv(dataframe, filename)
+    export_dataframe_to_html(dataframe, filename)
+
+
+def export_dataframe_to_csv(dataframe, filename):
     dataframe_to_csv_file(dataframe, filename + '.csv')
+    data_written = file_has_content(filename + '.csv')
+    if data_written:
+        export_finished(filename + '.csv')
+    else:
+        export_error()
+
+
+def export_dataframe_to_html(dataframe, filename):
     dataframe_to_html_file(dataframe, filename + '.html')
     prettify_html(filename + '.html')
+    data_written = file_has_content(filename + '.html')
+    if data_written:
+        export_finished(filename + '.html')
+    else:
+        export_error()
 
 
 def file_export(sender, data):
@@ -106,24 +223,28 @@ def file_export(sender, data):
         export_dataframe_to_files(all_grades, 'all-grades')
 
 
-def get_semester_grades(url):
-    content = browser.follow_link(link=url)
-    table = content.soup.find('table')
-    dataframe = table_to_dataframe(table)
-    dataframe['Note'] = interpret_current_grades(dataframe)
-    dataframe = dataframe.loc[:, ~dataframe.columns.str.contains('^Unnamed')]
+def file_export_csv(sender, data):
+    print('sender:')
+    print(sender)
+    print(data)
+    if sender == 'Export Semester Grades to CSV':
+        export_dataframe_to_csv(semester_grades, 'semester-grades')
+    elif sender == 'Export All Grades to CSV':
+        export_dataframe_to_csv(all_grades, 'all-grades')
 
-    global semester_grades
-    semester_grades = dataframe
 
-    add_table("Table##semester", ["Module", "Type", "Grade"], parent="Main Window")
-    set_table_data("Table##semester", dataframe_to_gui_table(dataframe))
-    add_button('Export Semester Grades to Files',
-               callback=file_export,
-               parent="Main Window", tip='Saves your grades to CSV and HTML')
+def file_export_html(sender, data):
+    print('sender:')
+    print(sender)
+    print(data)
+    if sender == 'Export Semester Grades to HTML':
+        export_dataframe_to_html(semester_grades, 'semester-grades')
+    elif sender == 'Export All Grades to HTML':
+        export_dataframe_to_html(all_grades, 'all-grades')
 
 
 def logout_link_present():
+    """Crude way to check for valid login on our side"""
     page = browser.page
     logout_link = page.select('a[href="portal.abmelden"]')
     if len(logout_link) > 0:
@@ -132,15 +253,20 @@ def logout_link_present():
         return False
 
 
+def get_sessions_token_url():
+    meta = browser.page.find("meta")
+    token = meta['content'][6:]
+    return token
+
+
 def login(user, password):
-    browser.open(url)
+    """Uses the provided credentials to log into VirtuOhm"""
+    browser.open(portal_url)
     browser.select_form('form[action="/pls/portal/portal.authorisation"]')
     browser["in_username"] = user
     browser["in_password"] = password
     browser.submit_selected()
-    meta = browser.page.find("meta")
-    token_url = meta['content'][6:]
-    response = browser.open(base_url + token_url)
+    browser.open(base_url + get_sessions_token_url())
     return logout_link_present()
 
 
@@ -153,19 +279,26 @@ def recursively_show(container):
             show_item(item)
 
 
-def download_current(sender, data):
+def download_semester_grades(sender, data):
     get_semester_grades(links['semester_grades'])
 
 
-def download_all(sender, data):
+def download_all_grades(sender, data):
     get_all_grades(links['all_grades'])
 
 
-def show_actions():
+def show_user_actions():
+    """After successful login these actions can be performed within VirtuOhm"""
+    add_dummy(parent="Main Window")
     add_text("You are now logged in.", parent="Main Window")
-    add_button('Semester Grades', callback=download_current, parent="Main Window")
+    add_dummy(parent="Main Window")
+    add_button('Semester Grades',
+               callback=download_semester_grades,
+               parent="Main Window")
     add_same_line(parent="Main Window")
-    add_button('All Grades', callback=download_all, parent="Main Window")
+    add_button('All Grades',
+               callback=download_all_grades,
+               parent="Main Window")
 
 
 def collect_links():
@@ -183,17 +316,41 @@ def try_login(sender, data):
     user = get_value('User')
     if password and user:
         login_valid = login(user, password)
-        print('login valid:')
-        print(login_valid)
         if login_valid:
             delete_item('Login Window')
             recursively_show('Main Window')
             collect_links()
-            show_actions()
+            show_user_actions()
         else:
             add_text('Incorrect Credentials!', color=[255, 0, 0], parent='Login Window')
     else:
         show_item('You have to enter a username and a password!')
+
+
+def position_export_error_window(sender, data):
+    if does_item_exist('Error saving file!'):
+        main_width = get_item_width('Main Window')
+        main_height = get_item_height('Main Window')
+        login_width = get_item_width('Error saving file!')
+        login_height = get_item_height('Error saving file!')
+        set_window_pos('Error saving file!', int((main_width / 2 - login_width / 2)),
+                       int((main_height / 2 - login_height / 2)))
+    else:
+        # this replaces the callback with None so we dont waste resources running the login window position callback
+        set_render_callback(None)
+
+
+def position_export_window(sender, data):
+    if does_item_exist('File Saved'):
+        main_width = get_item_width('Main Window')
+        main_height = get_item_height('Main Window')
+        login_width = get_item_width('File Saved')
+        login_height = get_item_height('File Saved')
+        set_window_pos('File Saved', int((main_width / 2 - login_width / 2)),
+                       int((main_height / 2 - login_height / 2)))
+    else:
+        # this replaces the callback with None so we dont waste resources running the login window position callback
+        set_render_callback(None)
 
 
 def position_login_window(sender, data):
